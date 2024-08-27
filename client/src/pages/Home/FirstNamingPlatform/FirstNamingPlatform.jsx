e06140a660d21546c4fe5ae630d31d940a565fe0
MrLipa2496 <sasalipa216@gmail.com>
2024-08-27 13:45:18 +0300

    Add FirstNamingPlatform styles

--- client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
+++ client/src/pages/Home/FirstNamingPlatform/FirstNamingPlatform.jsx
@@ -1,26 +1,27 @@
 import { Link } from 'react-router-dom';
 import data from './data';
 import CONSTANTS from '../../../constants';
+import styles from './FirstNamingPlatform.module.sass';
 
 const { STATIC_IMAGES_PATH } = CONSTANTS;
 
 function FirstNamingPlatform () {
   const mapItems = ({ icon, title, body }, i) => (
-    <li key={i}>
+    <li className={styles.item} key={i}>
       <img src={icon} alt='icon' />
-      <h3>{title}</h3>
-      <p>{body}</p>
+      <h3 className={styles.itemTitle}>{title}</h3>
+      <p className={styles.itemBody}>{body}</p>
     </li>
   );
 
   return (
     <div>
       <h2>Atom: The World's #1 Business Naming Platform</h2>
-      <ul>{data.map(mapItems)}</ul>
-      <div>
+      <ul className={styles.itemsWrapper}>{data.map(mapItems)}</ul>
+      <div className={styles.infoBtnWrapper}>
         <img src={`${STATIC_IMAGES_PATH}home/nh-theworld5.svg`} alt='icon' />
         <p>The Ultra Premium Marketplace Is Here</p>
-        <Link>Explore nowUltra Premium Marketplace</Link>
+        <Link className={styles.linkBtn}>Explore now</Link>
       </div>
     </div>
   );
