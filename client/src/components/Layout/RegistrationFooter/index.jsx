bc19325f8d7ed6e688ea42661e4ca58f6d6198b0
MrLipa2496 <sasalipa216@gmail.com>
2024-08-29 12:53:57 +0300

    Add data.json & refactoring

--- client/src/components/Layout/RegistrationFooter/index.jsx
+++ client/src/components/Layout/RegistrationFooter/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './RegistrationFooter.module.sass';
 import CONSTANTS from '../../../constants';
+import faqData from './data.json';
 
 const { TEL } = CONSTANTS.CONTACTS;
 
@@ -9,55 +10,14 @@ const RegistrationFooter = () => {
     <div className={styles.footer}>
       <div className={styles.articlesMainContainer}>
         <div className={styles.ColumnContainer}>
-          <div className={styles.headerArticle}>
-            Why should I use Squadhelp?
-          </div>
-          <div className={styles.article}>
-            You always have an option of hiring a consultant or coming up with
-            the name yourself. However, Squadhelp builds great brands that
-            succeed faster by connecting you with the most creative people
-            across the globe. Most importantly, Squadhelp provides you with
-            choice: you get to see ideas from dozens (in some cases, hundreds)
-            of contestants before you select a winner. Typically, you would
-            spend far less money with Squadhelp (our contests start at $199)
-            than hiring an agency. Also, you will receive immediate results -
-            most contests begin receiving submissions within minutes of
-            starting.
-          </div>
-          <div className={styles.headerArticle}>
-            How is Squadhelp Different?
-          </div>
-          <div className={styles.article}>
-            Since 2011, we have been committed to disrupting the traditional
-            agency model. Our platform offers much more than a typical
-            crowdsourcing experience. From Machine Learning to Audience Testing
-            to Comprehensive Trademark Validation, you receive best-in-class
-            support for your branding projects. Breadth: Our Contest-Based
-            Crowdsourcing approach allows you to receive an unmatched breadth of
-            name ideas from dozens of unique, creative minds while working with
-            the world's largest branding community. Quality and Collaboration:
-            Using an advanced Quality Scoring Algorithm, we ensure that you
-            receive more ideas from our top-quality creatives, and we use
-            Gamification best practices to encourage high-quality brainstorming
-            and two-way communication throughout your contest. We don’t stop at
-            ideation: Choose your name with confidence through our high-end
-            validation services. Poll your target demographics to get unbiased
-            feedback on your favorite names, and receive Trademark Risk and
-            Linguistics Analysis Reports developed by a Licensed Trademark
-            Attorney.
-          </div>
+          {faqData.map((item, index) => (
+            <div key={index}>
+              <div className={styles.headerArticle}>{item.question}</div>
+              <div className={styles.article}>{item.answer}</div>
+            </div>
+          ))}
         </div>
         <div className={styles.ColumnContainer}>
-          <div className={styles.headerArticle}>
-            I’ve never used Squadhelp before. What should I expect?
-          </div>
-          <div className={styles.article}>
-            Most customers tell us that Squadhelp’s process is effective, easy,
-            fast, and even fun. We constantly hear extremely positive feedback
-            with respect to the breadth of ideas submitted to each contest, and
-            many customers are surprised at how insightful working with dozens
-            of creative individuals from across the globe can be.
-          </div>
           <div className={styles.headerArticle}>How much does it cost?</div>
           <div className={styles.article}>
             Our naming competitions start at $199, and our logo design
